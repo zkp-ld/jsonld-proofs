81ef3484983ee373a5c7e1f61a6be2b2cfcdc71e
Dan Yamamoto <dan@iij.ad.jp>
2023-08-29 05:21:04 +0000

    remove jest

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-module.exports = {
-  preset: 'ts-jest/presets/js-with-ts',
-  testEnvironment: 'node',
-  roots: ["<rootDir>/tests"],
-  testPathIgnorePatterns: ["/node_modules/"],
-};
