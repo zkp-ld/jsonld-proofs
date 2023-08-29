050208a809bfd2b83866f38b7a0de97c8d11f92a
Dan Yamamoto <dan@iij.ad.jp>
2023-08-29 05:23:27 +0000

    update tests

--- tests/proofs.spec.ts
+++ tests/proofs.spec.ts
@@ -1,9 +1,11 @@
 import * as jsonld from 'jsonld';
 import disclosed0 from '../example/disclosed0.json';
+import disclosed0HiddenLiteral from '../example/disclosed0_hidden_literal.json';
 import disclosed1 from '../example/disclosed1.json';
 import keypairs from '../example/keypairs.json';
 import vcDraft0 from '../example/vc0.json';
 import vcDraft1 from '../example/vc1.json';
+import vp from '../example/vp.json';
 import _vpContext from '../example/vpContext.json';
 import { sign, deriveProof, verifyProof } from '../src/api';
 
@@ -30,4 +32,33 @@ describe('Proofs', () => {
     console.log(`verified: ${JSON.stringify(verified, null, 2)}`);
     expect(verified.verified).toBeTruthy();
   });
+
+  test('verifyProof', async () => {
+    const nonce = 'abcde';
+    const verified = await verifyProof(vp, nonce, keypairs);
+    console.log(`verified: ${JSON.stringify(verified, null, 2)}`);
+    expect(verified.verified).toBeTruthy();
+  });
+
+  test('deriveProof and verifyProof with hidden literal', async () => {
+    const vc0 = await sign(vcDraft0, keypairs);
+    const vc1 = await sign(vcDraft1, keypairs);
+    const nonce = 'abcde';
+    const vp = await deriveProof(
+      [
+        { vc: vc0, disclosed: disclosed0HiddenLiteral },
+        { vc: vc1, disclosed: disclosed1 },
+      ],
+      nonce,
+      keypairs,
+      vpContext,
+    );
+    console.log(`vp:\n${JSON.stringify(vp, null, 2)}`);
+    expect(vp).not.toHaveProperty('error');
+
+    const verified = await verifyProof(vp, nonce, keypairs);
+    console.log(`verified: ${JSON.stringify(verified, null, 2)}`);
+    expect(verified.verified).toBeTruthy();
+  });
+
 });
