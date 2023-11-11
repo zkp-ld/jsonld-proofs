bae52f68125d01e0b1e9fa09269e7ad22a00fc8d
Dan Yamamoto <dan@iij.ad.jp>
2023-11-11 09:22:22 +0000

    refactor JSON-LD diff processing (cont.)

--- src/api.ts
+++ src/api.ts
@@ -1,4 +1,3 @@
-/* eslint-disable no-restricted-syntax */ // TODO: remove this
 import {
   keyGen as keyGenWasm,
   sign as signWasm,
@@ -19,22 +18,17 @@ import * as jsonld from 'jsonld';
 import {
   DeriveProofOptions,
   DocumentLoader,
-  JsonValue,
   VC,
   VcPair,
   VerifyProofOptions,
 } from './types';
 import {
-  deskolemizeTerm,
   jsonldToRDF,
-  diffVC,
   jsonldVPFromRDF,
-  expandedVCToRDF,
   vcToRDF,
-  traverseJSON,
-  skolemizeVC,
   jsonldProofFromRDF,
-  deskolemizeString,
+  getRDFAndDeanonMaps,
+  getPredicatesRDF,
 } from './utils';
 
 export const keyGen = async (): Promise<KeyPair> => {
@@ -181,137 +175,16 @@ export const deriveProof = async (
 ): Promise<jsonld.JsonLdDocument> => {
   await initializeWasm();
 
-  const vcPairsRDF = [];
-  const deanonMap = new Map<string, string>();
+  const { vcPairsRDF, deanonMap } = await getRDFAndDeanonMaps(
+    vcPairs,
+    documentLoader,
+  );
+
   const publicKeysRDF = await jsonldToRDF(publicKeys, documentLoader);
 
-  const skolemizedPredicatesRDF = options?.predicates
-    ? await Promise.all(
-        options.predicates.map(async (predicate) => {
-          const expandedPredicate = await jsonld.expand(predicate, {
-            documentLoader,
-            safe: true,
-          });
-
-          return jsonldToRDF(
-            skolemizeVC(expandedPredicate, true),
-            documentLoader,
-          );
-        }),
-      )
+  const predicatesRDF = options?.predicates
+    ? await Promise.all(getPredicatesRDF(options.predicates, documentLoader))
     : undefined;
-  const predicatesRDF = skolemizedPredicatesRDF
-    ? skolemizedPredicatesRDF.map((predicate) => deskolemizeTerm(predicate))
-    : undefined;
-
-  for (const { original, disclosed } of vcPairs) {
-    const expandedOriginalVC = await jsonld.expand(original, {
-      documentLoader,
-      safe: true,
-    });
-    const expandedDisclosedVC = await jsonld.expand(disclosed, {
-      documentLoader,
-      safe: true,
-    });
-
-    const skolemizedExpandedOriginalVC = skolemizeVC(expandedOriginalVC, true);
-    const skolemizedExpandedDisclosedVC = skolemizeVC(
-      expandedDisclosedVC,
-      false,
-    );
-
-    // compare VC and disclosed VC to get local deanon map and skolem ID map
-    const {
-      deanonMap: localDeanonMap,
-      skolemIDMap,
-      maskedLiteralMap,
-    } = diffVC(skolemizedExpandedOriginalVC, skolemizedExpandedDisclosedVC);
-
-    // update global deanonMap
-    for (const [k, v] of localDeanonMap.entries()) {
-      if (deanonMap.has(k) && deanonMap.get(k) !== v) {
-        throw new Error(
-          `pseudonym \`${k}\` corresponds to multiple values: \`${JSON.stringify(
-            v,
-          )}\` and \`${JSON.stringify(deanonMap.get(k))}\``,
-        );
-      }
-      deanonMap.set(k, v);
-    }
-
-    // inject Skolem IDs into disclosed VC
-    for (const [path, skolemID] of skolemIDMap) {
-      const node = traverseJSON(
-        skolemizedExpandedDisclosedVC as JsonValue,
-        path,
-      );
-      node['@id'] = skolemID;
-    }
-
-    // inject masked Literal into disclosed VC
-    for (const path of maskedLiteralMap) {
-      const node = traverseJSON(
-        skolemizedExpandedDisclosedVC as JsonValue,
-        path,
-      );
-
-      const value = node['@value'];
-      if (typeof value !== 'string') {
-        throw new TypeError('invalid disclosed VC'); // TODO: more detail message
-      }
-
-      const typ = node['@type'];
-
-      // replace value node with id node
-      node['@id'] = value;
-      delete node['@type'];
-      delete node['@value'];
-
-      const deskolemizedValue = deskolemizeString(value);
-      const deanonMapEntry = deanonMap.get(deskolemizedValue);
-      if (deanonMapEntry === undefined) {
-        throw new Error(`deanonMap[${value}] has no value`);
-      }
-
-      if (typeof typ === 'string') {
-        deanonMap.set(deskolemizedValue, `${deanonMapEntry}^^<${typ}>`);
-      } else if (typ === undefined) {
-        deanonMap.set(deskolemizedValue, `${deanonMapEntry}`);
-      } else {
-        throw new TypeError('invalid disclosed VC'); // TODO: more detail message
-      }
-    }
-
-    // convert VC to N-Quads
-    const { documentRDF: skolemizedDocumentRDF, proofRDF: skolemizedProofRDF } =
-      await expandedVCToRDF(skolemizedExpandedOriginalVC, documentLoader);
-
-    // convert disclosed VC to N-Quads
-    const {
-      documentRDF: skolemizedDisclosedDocumentRDF,
-      proofRDF: skolemizedDisclosedProofRDF,
-    } = await expandedVCToRDF(skolemizedExpandedDisclosedVC, documentLoader);
-
-    // deskolemize N-Quads
-    const [
-      originalDocumentRDF,
-      originalProofRDF,
-      disclosedDocumentRDF,
-      disclosedProofRDF,
-    ] = [
-      skolemizedDocumentRDF,
-      skolemizedProofRDF,
-      skolemizedDisclosedDocumentRDF,
-      skolemizedDisclosedProofRDF,
-    ].map(deskolemizeTerm);
-
-    vcPairsRDF.push({
-      originalDocument: originalDocumentRDF,
-      originalProof: originalProofRDF,
-      disclosedDocument: disclosedDocumentRDF,
-      disclosedProof: disclosedProofRDF,
-    });
-  }
 
   const vp = deriveProofWasm({
     vcPairs: vcPairsRDF,
--- src/utils.ts
+++ src/utils.ts
@@ -1,9 +1,8 @@
-/* eslint-disable no-restricted-syntax */ // TODO: remove this
 import { diff } from 'json-diff';
 import * as jsonld from 'jsonld';
 import * as jsonldSpec from 'jsonld/jsonld-spec';
 import { customAlphabet } from 'nanoid';
-import { DocumentLoader, JsonValue, VC, VCDocument } from './types';
+import { DocumentLoader, JsonValue, VC, VCDocument, VcPair } from './types';
 
 const PROOF = 'https://w3id.org/security#proof';
 const DATA_INTEGRITY_CONTEXT = 'https://www.w3.org/ns/data-integrity/v1';
@@ -25,56 +24,87 @@ export const jsonldToRDF = async (
     safe: true,
   })) as unknown as string;
 
-export const vcToRDF = async (vc: VC, documentLoader: DocumentLoader) => {
-  const clonedVC = JSON.parse(JSON.stringify(vc)) as VC;
-
-  const { proof } = clonedVC;
-  const document = clonedVC as VCDocument;
-  delete document.proof;
-
-  if (!('@context' in proof)) {
-    proof['@context'] = DATA_INTEGRITY_CONTEXT;
+const skolemizeJSONLD = (node: JsonValue, includeOmittedId: boolean) => {
+  if (Array.isArray(node)) {
+    node.forEach((item) => {
+      if (typeof item === 'object' && item !== null) {
+        skolemizeJSONLD(item, includeOmittedId);
+      }
+    });
+  } else if (typeof node === 'object' && node !== null) {
+    for (const key in node) {
+      if (
+        typeof node[key] === 'object' &&
+        node[key] !== undefined &&
+        key !== '@context'
+      ) {
+        skolemizeJSONLD(node[key], includeOmittedId);
+      } else {
+        const value = node[key];
+        if (typeof value === 'string' && value.startsWith('_:')) {
+          node[key] = `${SKOLEM_PREFIX}${value.slice(2)}`;
+        }
+      }
+    }
+    if (
+      includeOmittedId &&
+      !('@value' in node || '@id' in node || '@list' in node)
+    ) {
+      node['@id'] = `${SKOLEM_PREFIX}${nanoid()}`;
+    }
   }
-
-  const documentRDF = await jsonldToRDF(document, documentLoader);
-  const proofRDF = await jsonldToRDF(proof, documentLoader);
-
-  return { document, documentRDF, proof, proofRDF };
 };
 
-export const expandedVCToRDF = async (
-  vc: jsonld.NodeObject[],
+// input `vc` must be *expanded* JSON-LD
+const skolemizeVC = (
+  vc: jsonldSpec.JsonLdArray | jsonld.JsonLdDocument,
+  includeOmittedId?: boolean,
+) => {
+  const output = JSON.parse(JSON.stringify(vc)) as JsonValue;
+  skolemizeJSONLD(
+    output,
+    includeOmittedId === undefined ? true : includeOmittedId,
+  );
+
+  return output as jsonldSpec.JsonLdArray;
+};
+
+const skolemizeAndExpandVcPair = async (
+  vcPair: VcPair,
   documentLoader: DocumentLoader,
 ) => {
-  const clonedVC = JSON.parse(JSON.stringify(vc)) as jsonld.NodeObject[];
+  const expandedOriginalVC = await jsonld.expand(vcPair.original, {
+    documentLoader,
+    safe: true,
+  });
+  const expandedDisclosedVC = await jsonld.expand(vcPair.disclosed, {
+    documentLoader,
+    safe: true,
+  });
 
-  if (
-    !(PROOF in clonedVC[0]) ||
-    !Array.isArray(clonedVC[0][PROOF]) ||
-    typeof clonedVC[0][PROOF][0] !== 'object' ||
-    clonedVC[0][PROOF][0] === null ||
-    !('@graph' in clonedVC[0][PROOF][0]) ||
-    !Array.isArray(clonedVC[0][PROOF][0]['@graph'])
-  ) {
-    throw new TypeError('VC must have proof');
-  }
-
-  if (clonedVC[0][PROOF][0]['@graph'].length > 1) {
-    throw new TypeError('VC must have single proof');
-  }
-
-  const proof = clonedVC[0][PROOF][0]['@graph'][0];
-
-  if (typeof proof !== 'object' || proof === null || Array.isArray(proof)) {
-    throw new TypeError('invalid VC');
-  }
-  delete clonedVC[0][PROOF];
-
-  const documentRDF = await jsonldToRDF(clonedVC, documentLoader);
-  const proofRDF = await jsonldToRDF(proof, documentLoader);
-
-  return { documentRDF, proofRDF };
+  return {
+    original: skolemizeVC(expandedOriginalVC),
+    disclosed: skolemizeVC(expandedDisclosedVC, false),
+  };
 };
+
+export const getPredicatesRDF = (
+  predicates: jsonld.JsonLdDocument[],
+  documentLoader: DocumentLoader,
+) =>
+  predicates.map(async (predicate) => {
+    const expandedPredicate = await jsonld.expand(predicate, {
+      documentLoader,
+      safe: true,
+    });
+    const skolemizedAndExpandedPredicate = skolemizeVC(expandedPredicate);
+    const skolemizedAndExpandedPredicateRDF = await jsonldToRDF(
+      skolemizedAndExpandedPredicate,
+      documentLoader,
+    );
+
+    return deskolemizeTerm(skolemizedAndExpandedPredicateRDF);
+  });
 
 const diffJSONLD = (
   node: JsonValue,
@@ -154,9 +184,7 @@ const diffJSONLD = (
           skolemIDMap.set(path, `${SKOLEM_PREFIX}${masked}`);
           deanonMap.set(`_:${masked}`, `<${value}>`);
         }
-      } else if (key.endsWith('__deleted')) {
-        continue;
-      } else {
+      } else if (!key.endsWith('__deleted')) {
         const updatedPath = path.concat([key]);
         const value = node[key];
         if (typeof value === 'object') {
@@ -175,7 +203,7 @@ const diffJSONLD = (
   return {};
 };
 
-export const diffVC = (
+const diffVC = (
   vc: jsonld.JsonLdDocument,
   disclosed: jsonld.JsonLdDocument,
 ) => {
@@ -186,49 +214,197 @@ export const diffVC = (
 
   diffJSONLD(diffObj, [], deanonMap, skolemIDMap, maskedLiteralPaths);
 
-  return { deanonMap, skolemIDMap, maskedLiteralMap: maskedLiteralPaths };
+  return { deanonMap, skolemIDMap, maskedLiteralPaths };
 };
 
-const skolemizeJSONLD = (node: JsonValue, includeOmittedId: boolean) => {
-  if (Array.isArray(node)) {
-    node.forEach((item) => {
-      if (typeof item === 'object' && item !== null) {
-        skolemizeJSONLD(item, includeOmittedId);
+const traverseJSON = (root: JsonValue, path: (string | number)[]) => {
+  let node = root;
+
+  for (const item of path) {
+    if (Array.isArray(node)) {
+      if (typeof item !== 'number') {
+        throw new Error(
+          'internal error when injecting skolem IDs to disclosed VC',
+        );
       }
-    });
-  } else if (typeof node === 'object' && node !== null) {
-    for (const key in node) {
-      if (
-        typeof node[key] === 'object' &&
-        node[key] !== undefined &&
-        key !== '@context'
-      ) {
-        skolemizeJSONLD(node[key], includeOmittedId);
-      } else {
-        const value = node[key];
-        if (typeof value === 'string' && value.startsWith('_:')) {
-          node[key] = `${SKOLEM_PREFIX}${value.slice(2)}`;
-        }
+      node = node[item];
+    } else if (typeof node === 'object' && node !== null) {
+      if (typeof item !== 'string') {
+        throw new Error(
+          'internal error when injecting skolem IDs to disclosed VC',
+        );
       }
-    }
-    if (
-      includeOmittedId &&
-      !('@value' in node || '@id' in node || '@list' in node)
-    ) {
-      node['@id'] = `${SKOLEM_PREFIX}${nanoid()}`;
+      node = node[item];
+    } else {
+      throw new Error(
+        'internal error when injecting skolem IDs to disclosed VC',
+      );
     }
   }
+
+  if (typeof node !== 'object' || node === null || Array.isArray(node)) {
+    throw new Error('internal error when injecting skolem IDs to disclosed VC');
+  }
+
+  return node;
 };
 
-// input `vc` must be *expanded* JSON-LD
-export const skolemizeVC = (
-  vc: jsonldSpec.JsonLdArray | jsonld.JsonLdDocument,
-  includeOmittedId: boolean,
+const expandedVCToRDF = async (
+  vc: jsonld.NodeObject[],
+  documentLoader: DocumentLoader,
 ) => {
-  const output = JSON.parse(JSON.stringify(vc)) as JsonValue;
-  skolemizeJSONLD(output, includeOmittedId);
+  const clonedVC = JSON.parse(JSON.stringify(vc)) as jsonld.NodeObject[];
 
-  return output as jsonldSpec.JsonLdArray;
+  if (
+    !(PROOF in clonedVC[0]) ||
+    !Array.isArray(clonedVC[0][PROOF]) ||
+    typeof clonedVC[0][PROOF][0] !== 'object' ||
+    clonedVC[0][PROOF][0] === null ||
+    !('@graph' in clonedVC[0][PROOF][0]) ||
+    !Array.isArray(clonedVC[0][PROOF][0]['@graph'])
+  ) {
+    throw new TypeError('VC must have proof');
+  }
+
+  if (clonedVC[0][PROOF][0]['@graph'].length > 1) {
+    throw new TypeError('VC must have single proof');
+  }
+
+  const proof = clonedVC[0][PROOF][0]['@graph'][0];
+
+  if (typeof proof !== 'object' || proof === null || Array.isArray(proof)) {
+    throw new TypeError('invalid VC');
+  }
+  delete clonedVC[0][PROOF];
+
+  const documentRDF = await jsonldToRDF(clonedVC, documentLoader);
+  const proofRDF = await jsonldToRDF(proof, documentLoader);
+
+  return { documentRDF, proofRDF };
+};
+
+export const getRDFAndDeanonMaps = async (
+  vcPairs: VcPair[],
+  documentLoader: DocumentLoader,
+) => {
+  const skolemizedAndExpandedVcPairs = await Promise.all(
+    vcPairs.map((vcPair) => skolemizeAndExpandVcPair(vcPair, documentLoader)),
+  );
+
+  // compare VC and disclosed VC to get local deanon map and skolem ID map
+  const diffObjs = skolemizedAndExpandedVcPairs.map(({ original, disclosed }) =>
+    diffVC(original, disclosed),
+  );
+
+  // aggregate deanon maps
+  const deanonMap = new Map<string, string>();
+  diffObjs.forEach(({ deanonMap: localDeanonMap }) => {
+    localDeanonMap.forEach((value, key) => {
+      if (deanonMap.has(key) && deanonMap.get(key) !== value) {
+        throw new Error(
+          `pseudonym \`${key}\` corresponds to multiple values: \`${JSON.stringify(
+            value,
+          )}\` and \`${JSON.stringify(deanonMap.get(key))}\``,
+        );
+      }
+      deanonMap.set(key, value);
+    });
+  });
+
+  // update disclosed VCs
+  skolemizedAndExpandedVcPairs.forEach(({ disclosed }, i) => {
+    // copy Skolem IDs from original VC into disclosed VC
+    diffObjs[i].skolemIDMap.forEach((skolemID, path) => {
+      const node = traverseJSON(disclosed as JsonValue, path);
+      node['@id'] = skolemID;
+    });
+
+    // inject masked Literal into disclosed VC
+    diffObjs[i].maskedLiteralPaths.forEach((path) => {
+      const node = traverseJSON(disclosed as JsonValue, path);
+
+      const value = node['@value'];
+      if (typeof value !== 'string') {
+        throw new TypeError('invalid disclosed VC'); // TODO: more detail message
+      }
+
+      const typ = node['@type'];
+
+      // replace value node with id node
+      node['@id'] = value;
+      delete node['@type'];
+      delete node['@value'];
+
+      const deskolemizedValue = deskolemizeString(value);
+      const deanonMapEntry = deanonMap.get(deskolemizedValue);
+      if (deanonMapEntry === undefined) {
+        throw new Error(`deanonMap[${value}] has no value`);
+      }
+
+      if (typeof typ === 'string') {
+        deanonMap.set(deskolemizedValue, `${deanonMapEntry}^^<${typ}>`);
+      } else if (typ === undefined) {
+        deanonMap.set(deskolemizedValue, `${deanonMapEntry}`);
+      } else {
+        throw new TypeError('invalid disclosed VC'); // TODO: more detail message
+      }
+    });
+  });
+
+  const vcPairsRDF = await Promise.all(
+    skolemizedAndExpandedVcPairs.map(async ({ original, disclosed }) => {
+      // convert VC to N-Quads
+      const {
+        documentRDF: skolemizedDocumentRDF,
+        proofRDF: skolemizedProofRDF,
+      } = await expandedVCToRDF(original, documentLoader);
+
+      // convert disclosed VC to N-Quads
+      const {
+        documentRDF: skolemizedDisclosedDocumentRDF,
+        proofRDF: skolemizedDisclosedProofRDF,
+      } = await expandedVCToRDF(disclosed, documentLoader);
+
+      // deskolemize N-Quads
+      const [
+        originalDocument,
+        originalProof,
+        disclosedDocument,
+        disclosedProof,
+      ] = [
+        skolemizedDocumentRDF,
+        skolemizedProofRDF,
+        skolemizedDisclosedDocumentRDF,
+        skolemizedDisclosedProofRDF,
+      ].map(deskolemizeTerm);
+
+      return {
+        originalDocument,
+        originalProof,
+        disclosedDocument,
+        disclosedProof,
+      };
+    }),
+  );
+
+  return { vcPairsRDF, deanonMap };
+};
+
+export const vcToRDF = async (vc: VC, documentLoader: DocumentLoader) => {
+  const clonedVC = JSON.parse(JSON.stringify(vc)) as VC;
+
+  const { proof } = clonedVC;
+  const document = clonedVC as VCDocument;
+  delete document.proof;
+
+  if (!('@context' in proof)) {
+    proof['@context'] = DATA_INTEGRITY_CONTEXT;
+  }
+
+  const documentRDF = await jsonldToRDF(document, documentLoader);
+  const proofRDF = await jsonldToRDF(proof, documentLoader);
+
+  return { document, documentRDF, proof, proofRDF };
 };
 
 export const jsonldProofFromRDF = async (
@@ -288,36 +464,4 @@ export const jsonldVPFromRDF = async (
   });
 
   return out;
-};
-
-export const traverseJSON = (root: JsonValue, path: (string | number)[]) => {
-  let node = root;
-
-  for (const item of path) {
-    if (Array.isArray(node)) {
-      if (typeof item !== 'number') {
-        throw new Error(
-          'internal error when injecting skolem IDs to disclosed VC',
-        );
-      }
-      node = node[item];
-    } else if (typeof node === 'object' && node !== null) {
-      if (typeof item !== 'string') {
-        throw new Error(
-          'internal error when injecting skolem IDs to disclosed VC',
-        );
-      }
-      node = node[item];
-    } else {
-      throw new Error(
-        'internal error when injecting skolem IDs to disclosed VC',
-      );
-    }
-  }
-
-  if (typeof node !== 'object' || node === null || Array.isArray(node)) {
-    throw new Error('internal error when injecting skolem IDs to disclosed VC');
-  }
-
-  return node;
 };
