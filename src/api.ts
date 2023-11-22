19333a708f252c1c374265b467a7c5cd71ae01d2
Dan Yamamoto <dan@iij.ad.jp>
2023-11-22 10:36:38 +0000

    Add comments for API functions

--- src/api.ts
+++ src/api.ts
@@ -1,3 +1,11 @@
+/**
+ * This file contains the API functions for working with JSON-LD proofs.
+ * These functions provide functionality for key generation, signing and verification of Verifiable Credentials (VCs),
+ * blind signing, unblinding, deriving proofs, and verifying proofs.
+ * The functions utilize the @zkp-ld/rdf-proofs-wasm library for low-level cryptographic operations.
+ * @packageDocumentation
+ */
+
 import {
   keyGen as keyGenWasm,
   sign as signWasm,
@@ -31,6 +39,10 @@ import {
   getPredicatesRDF,
 } from './utils';
 
+/**
+ * Generates a key pair for signing and verification.
+ * @returns A Promise that resolves to the generated key pair.
+ */
 export const keyGen = async (): Promise<KeyPair> => {
   await initializeWasm();
 
@@ -39,6 +51,13 @@ export const keyGen = async (): Promise<KeyPair> => {
   return keypair;
 };
 
+/**
+ * Signs a Verifiable Credential (VC) using a given key pair and document loader.
+ * @param vc The Verifiable Credential to sign.
+ * @param keyPair The key pair used for signing.
+ * @param documentLoader The document loader used for resolving JSON-LD documents.
+ * @returns A Promise that resolves to the signed Verifiable Credential.
+ */
 export const sign = async (
   vc: VC,
   keyPair: jsonld.JsonLdDocument,
@@ -59,6 +78,13 @@ export const sign = async (
   return document as VC;
 };
 
+/**
+ * Verifies the signature of a Verifiable Credential (VC) using a given public key and document loader.
+ * @param vc The Verifiable Credential to verify.
+ * @param publicKey The public key used for verification.
+ * @param documentLoader The document loader used for resolving JSON-LD documents.
+ * @returns A Promise that resolves to the verification result.
+ */
 export const verify = async (
   vc: VC,
   publicKey: jsonld.JsonLdDocument,
@@ -76,6 +102,13 @@ export const verify = async (
   return verified;
 };
 
+/**
+ * Requests a blind signature for a given secret.
+ * @param secret The secret to be blind signed.
+ * @param challenge (Optional) The challenge string for the blind signature.
+ * @param skipPok (Optional) Whether to skip the proof of knowledge (PoK) step.
+ * @returns A Promise that resolves to the blind sign request.
+ */
 export const requestBlindSign = async (
   secret: Uint8Array,
   challenge?: string,
@@ -88,6 +121,13 @@ export const requestBlindSign = async (
   return request;
 };
 
+/**
+ * Verifies a blind sign request.
+ * @param commitment The commitment string from the blind sign request.
+ * @param pokForCommitment The proof of knowledge (PoK) for the commitment.
+ * @param challenge The challenge string used in the blind sign request.
+ * @returns A Promise that resolves to the verification result.
+ */
 export const verifyBlindSignRequest = async (
   commitment: string,
   pokForCommitment: string,
@@ -104,6 +144,14 @@ export const verifyBlindSignRequest = async (
   return verified;
 };
 
+/**
+ * Performs blind signing of a Verifiable Credential (VC) using a given commitment, key pair, and document loader.
+ * @param commitment The commitment string from the blind sign request.
+ * @param vc The Verifiable Credential to blind sign.
+ * @param keyPair The key pair used for blind signing.
+ * @param documentLoader The document loader used for resolving JSON-LD documents.
+ * @returns A Promise that resolves to the blind signed Verifiable Credential.
+ */
 export const blindSign = async (
   commitment: string,
   vc: VC,
@@ -130,6 +178,13 @@ export const blindSign = async (
   return document as VC;
 };
 
+/**
+ * Performs unblinding of a Verifiable Credential (VC) using a given blinding factor and document loader.
+ * @param vc The Verifiable Credential to unblind.
+ * @param blinding The blinding factor used for unblinding.
+ * @param documentLoader The document loader used for resolving JSON-LD documents.
+ * @returns A Promise that resolves to the unblinded Verifiable Credential.
+ */
 export const unblind = async (
   vc: VC,
   blinding: string,
@@ -148,6 +203,14 @@ export const unblind = async (
   return document as VC;
 };
 
+/**
+ * Verifies a blind signature of a Verifiable Credential (VC) using a given secret, public key, and document loader.
+ * @param secret The secret used for blind verification.
+ * @param vc The Verifiable Credential to blind verify.
+ * @param publicKey The public key used for blind verification.
+ * @param documentLoader The document loader used for resolving JSON-LD documents.
+ * @returns A Promise that resolves to the verification result.
+ */
 export const blindVerify = async (
   secret: Uint8Array,
   vc: VC,
@@ -166,6 +229,15 @@ export const blindVerify = async (
   return verified;
 };
 
+/**
+ * Derives a Verifiable Presentation (VP) from a set of Verifiable Credential (VC) pairs using a given set of public keys, context, and document loader.
+ * @param vcPairs The array of Verifiable Credential (VC) pairs, where each pair is an array of two VCs: the original VC and the partially-anonymized VC.
+ * @param publicKeys The public keys used for deriving the proof.
+ * @param context The JSON-LD context definition.
+ * @param documentLoader The document loader used for resolving JSON-LD documents.
+ * @param options (Optional) Additional options for deriving the proof.
+ * @returns A Promise that resolves to the derived proof as a JSON-LD document.
+ */
 export const deriveProof = async (
   vcPairs: VcPair[],
   publicKeys: jsonld.JsonLdDocument,
@@ -204,6 +276,14 @@ export const deriveProof = async (
   return jsonldVP;
 };
 
+/**
+ * Verifies a Verifiable Presentation (VP) using a given set of public keys, document loader, and additional options.
+ * @param vp The Verifiable Presentation (VP) to verify.
+ * @param publicKeys The public keys used for verification.
+ * @param documentLoader The document loader used for resolving JSON-LD documents.
+ * @param options (Optional) Additional options for verifying the proof.
+ * @returns A Promise that resolves to the verification result.
+ */
 export const verifyProof = async (
   vp: jsonld.JsonLdDocument,
   publicKeys: jsonld.JsonLdDocument,
